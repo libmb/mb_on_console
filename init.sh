cbd6e0daf4a0eaa82ad496a91f43ed90ee95c148
맹주영 <mjy9088@naver.com>
2024-04-08 02:29:28 +0900

    update dependencies

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+set -e
+
 git submodule init
+git submodule update
 ls submodules | xargs -I {} sh -c "cd submodules/{} && make"
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 00:30:55 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2024/03/14 00:37:39 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2024/04/08 02:29:02 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static size_t	get_iteration_count(
 {
 	t_mb_real	*r;
 	t_mb_real	*i;
-	t_err		result;
+	t_mb_err	result;
 
 	r = mb_real_float_new((float)x * 4 / ((float)WIDTH - 1) - 3);
 	if (!r)
