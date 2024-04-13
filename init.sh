96ce47b4a52f009e1689273d55c1f78bd3c00cf9
Juyeong Maing <mjy9088@naver.com>
2024-04-14 03:49:24 +0900

    add Windows support

--- init.sh
+++ init.sh
@@ -4,4 +4,7 @@ set -e
 
 git submodule init
 git submodule update
-ls submodules | xargs -I {} sh -c "cd submodules/{} && make"
+ls submodules | xargs -I {} sh -c "cd submodules/{} && sh build.sh"
+mkdir -p lib
+
+sh build/misc/compile_commands.json.init.sh
